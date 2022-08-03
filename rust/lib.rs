baebbd908bfe423d30ae570edbc07c528485d0e1
Quinn1876 <qhodges0442@gmail.com>
2022-08-03 22:14:06 +0000

    Added Flatbuffers - Added Error Types for UdpErrno fbs def - Added Pod Data fbs def - Added Pod state message fbs def

--- rust/lib.rs
+++ rust/lib.rs
@@ -7,8 +7,19 @@ pub mod pod_state_generated;
 pub use pod_state_generated as pod_state;
 
 #[allow(dead_code, unused_imports)]
+pub mod pod_data_generated;
+pub use pod_data_generated as pod_data;
+
+#[allow(dead_code, unused_imports)]
 pub mod udp_desktop_messages_generated;
 pub use udp_desktop_messages_generated as udp_desktop_message;
+
+#[allow(dead_code, unused_imports)]
+pub mod udp_pod_state_message_generated;
+pub use udp_pod_state_message_generated as udp_pod_state_message;
+
+#[allow(dead_code, unused_imports)]
+pub mod error_types_generated;
 
 /// Test Module for the flatbuffers, also shows example usages
 mod test {
@@ -21,6 +32,24 @@ mod test {
     udp_message_builder.add_most_recent_timestamp(1000);
     udp_message_builder.add_requested_state(pod_state::PodState::LowVoltage);
     let udp_message_offset = udp_message_builder.finish();
+    fbb.finish(udp_message_offset, None);
+    let udp_message_bytes = fbb.finished_data();
+    // THis is where data would normally go onto the wire
+    let udp_message = udp_desktop_message::root_as_udp_desktop_message(udp_message_bytes).unwrap();
+    assert_eq!(udp_message.most_recent_timestamp(), 1000);
+    assert_eq!(udp_message.requested_state(), pod_state::PodState::LowVoltage);
+  }
+
+  #[test]
+  fn test_udp_desktop_message_fast_creation() {
+    let mut fbb = flatbuffers::FlatBufferBuilder::with_capacity(128);
+    let udp_message_offset = udp_desktop_message::UdpDesktopMessage::create(
+      &mut fbb,
+      &udp_desktop_message::UdpDesktopMessageArgs {
+        requested_state: pod_state::PodState::LowVoltage,
+        most_recent_timestamp: 1000
+      }
+    );
     fbb.finish(udp_message_offset, None);
     let udp_message_bytes = fbb.finished_data();
     // THis is where data would normally go onto the wire
--- scripts/compile-flatbuffers.sh
+++ scripts/compile-flatbuffers.sh
@@ -1,2 +1,5 @@
 flatc --rust -o rust ./buffers/udp_desktop_messages.fbs
+flatc --rust -o rust ./buffers/udp_pod_state_message.fbs
 flatc --rust -o rust ./buffers/pod_state.fbs
+flatc --rust -o rust ./buffers/pod_data.fbs
+flatc --rust -o rust ./buffers/error_types.fbs
